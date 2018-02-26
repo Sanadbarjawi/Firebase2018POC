c26ee0458984f786e422929d1c3a1673efb7f27f
Sanad Barjawi <Sanadbarj@gmail.com>
2018-02-26 11:28:05 +0200

    Project initiating

--- Firebase2018POC/AppDelegate.swift
+++ Firebase2018POC/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let main = Firestore(nibName: "Firestore", bundle: nil)
+        let nav = UINavigationController()
+        window?.rootViewController = nav
+        nav.viewControllers = [main]
+        nav.isNavigationBarHidden = true
+        window?.makeKeyAndVisible()
         return true
     }
 
---
+++ Firebase2018POC/Firestore.swift
@@ -0,0 +1,20 @@
+//
+//  Firestore.swift
+//  Firebase2018POC
+//
+//  Created by Sanad  on 2/26/18.
+//  Copyright © 2018 Sanad . All rights reserved.
+//
+
+import UIKit
+import Firebase
+class Firestore: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
